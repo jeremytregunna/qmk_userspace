885129c0f65a67867dea9f62f9bf0524e7a3aca7
Jeremy Tregunna <jeremy@tregunna.ca>
2025-09-26 17:04:02 -0600

    fix layer buttons

--- keyboards/bastardkb/charybdis/4x6/keymaps/canoozie/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/canoozie/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LGUI,    PT_Z,    KC_X,    KC_M,    KC_C,    KC_V,       KC_K,    KC_L, KC_COMM,  KC_DOT, PT_SLSH, KC_LALT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                   KC_SPC, KC_LALT,   RAISE,      LOWER,  KC_ENT,
+                                   KC_SPC, KC_LALT,   RAISE,      KC_TOGG,  LOWER,
                                            KC_LSFT, KC_BSPC,     KC_DEL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
